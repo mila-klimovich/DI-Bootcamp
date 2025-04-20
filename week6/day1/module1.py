04714fe2f89781b5f925fa2f320cebfe4ea93a03
Mila Klimovich <mila.k.klimovich@gmail.com>
2025-04-20 18:56:16 +0300

    week6 day1 task1

---
+++ week6/day1/module1.py
@@ -0,0 +1,24 @@
+import sqlite3
+conn = sqlite3.connect('database.sqlite')
+cursor = conn.cursor()
+print("Opened database successfully")
+
+cursor.execute('''CREATE TABLE EMPLOYEE
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL);''')
+cursor.close()
+
+cursor.execute("INSERT INTO EMPLOYEE (ID,NAME,AGE)  VALUES (1, 'Razi', 14)");
+cursor.execute("INSERT INTO EMPLOYEE (ID,NAME,AGE)  VALUES (2, 'Jon', 19)");
+cursor.execute("INSERT INTO EMPLOYEE (ID,NAME,AGE)  VALUES (3, 'Martha', 35)");
+conn.execute("DELETE from EMPLOYEE where ID = 2")
+cursor.execute("SELECT * FROM EMPLOYEE")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conn.commit()
+
+cursor.close()
+conn.close()
